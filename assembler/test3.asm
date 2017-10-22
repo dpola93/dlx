bbf056992044883b95888bf6c73d1b06ab3adc89
Davide Pola <davide.pola@studenti.polito.it>
2017-10-23 00:47:43 +0200

    clean

--- assembler/test3.asm
+++ assembler/test3.asm
@@ -1,36 +1,39 @@
 ;nop
-sub r1,r0,r1	; r10 <- -1
-;seq r10,r1,r1 	; r10 <-  1
-;seq r10,r1,r2	; r10 <-  0
-;seq r10,r2,r2	; r10 <-  1
-;seq r10,r2,r1	; r10 <-  0
-;sne r10,r1,r1	; r10 <-  0
-;sne r10,r1,r2	; r10 <-  1
-;sne r10,r2,r2	; r10 <-  0
-;sne r10,r2,r1	; r10 <-  1
-;nop
-;sgt r10,r0,r0	; r10 <-  0
-;sge r10,r0,r0	; r10 <-  1
-;slt r10,r0,r0	; r10 <-  0
-;sle r10,r0,r0	; r10 <-  1
-;sgt r10,r1,r1	; r10 <-  0
-;sge r10,r1,r1	; r10 <-  1
-;slt r10,r1,r1	; r10 <-  0
-;sle r10,r1,r1	; r10 <-  1
-;sgt r10,r2,r2	; r10 <-  0
-;sge r10,r2,r2	; r10 <-  1
-;slt r10,r2,r2	; r10 <-  0
-;sle r10,r2,r2	; r10 <-  1
+addi r2,r0,2
+addi r1,r0,-1	; r10 <- -1
+addi r4,r0,10	; r10 <- -1
+seq r10,r1,r1 	; r10 <-  1
+seq r10,r1,r2	; r10 <-  0
+seq r10,r2,r2	; r10 <-  1
+seq r10,r2,r1	; r10 <-  0
+sne r10,r1,r1	; r10 <-  0
+sne r10,r1,r2	; r10 <-  1
+sne r10,r2,r2	; r10 <-  0
+sne r10,r2,r1	; r10 <-  1
 nop
-sgt r10,r0,r1	; r10 <-  0
-sge r10,r0,r1	; r10 <-  0
-slt r10,r0,r1	; r10 <-  1
-sle r10,r0,r1	; r10 <-  1
-sgt r10,r1,r0	; r10 <-  1
-sge r10,r1,r0	; r10 <-  1
-slt r10,r1,r0	; r10 <-  0
-sle r10,r1,r0	; r10 <-  0
-sgt r10,r1,r2	; r10 <-  1
-sge r10,r1,r2	; r10 <-  1
-slt r10,r1,r2	; r10 <-  0
-sle r10,r1,r2	; r10 <-  0
+sgt r10,r0,r0	; r10 <-  0
+sge r10,r0,r0	; r10 <-  1
+slt r10,r0,r0	; r10 <-  0
+sle r10,r0,r0	; r10 <-  1
+sgt r10,r1,r1	; r10 <-  0
+sge r10,r1,r1	; r10 <-  1
+slt r10,r1,r1	; r10 <-  0
+sle r10,r1,r1	; r10 <-  1
+sgt r10,r2,r2	; r10 <-  0
+sge r10,r2,r2	; r10 <-  1
+slt r10,r2,r2	; r10 <-  0
+sle r10,r2,r2	; r10 <-  1
+nop
+sgt r10,r0,r1	; r10 <-  1
+sgtu r10,r0,r1
+sge r10,r0,r1	; r10 <-  1
+slt r10,r0,r1	; r10 <-  0
+sle r10,r0,r1	; r10 <-  0
+sgt r10,r1,r0	; r10 <-  0
+sge r10,r1,r0	; r10 <-  0
+slt r10,r1,r0	; r10 <-  1
+sle r10,r1,r0	; r10 <-  1
+sgt r10,r1,r2	; r10 <-  0
+sge r10,r1,r2	; r10 <-  0
+slt r10,r1,r2	; r10 <-  1
+sle r10,r1,r2	; r10 <-  1
---
+++ assembler/test8.asm
@@ -0,0 +1,16 @@
+addi r1,r0,1
+addi r2,r0,-2
+addi r3,r0,3
+addi r4,r0,-32767
+addi r5,r0,5
+addi r10,r0,10
+nop
+multu r31,r1,r2 
+nop
+multu r31,r1,r3 
+nop
+multu r31,r1,r4 
+mult r31,r1,r2 
+mult r31,r1,r3 
+mult r31,r1,r4 
+nop
